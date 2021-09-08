efb38ee3c5b8220a625e21df812fd7b1cce3421c
Last-Railgun <3138304458@qq.com>
2021-09-08 22:25:21 +0800

    显示地图

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import BaiduMap from 'vue-baidu-map'
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+
+Vue.use(BaiduMap, {
+  ak: 'z35G6E5cSbdswS79YwumifAZwqXLmokb'
+})
 
 new Vue({
   router,
