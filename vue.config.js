e0685e1b75e0fdb2549ef21e884b467c4defb815
Last-Railgun <3138304458@qq.com>
2021-09-08 23:54:31 +0800

    显示地图,更改初始位置为当前定位(动态获取)

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+const webpack = require('webpack')
+
+module.exports = {
+    //import JQuery
+    chainWebpack: config => {
+        config.plugin('provide').use(webpack.ProvidePlugin, [{
+            $: 'jquery',
+            jquery: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        }])
+    }
+}
