99f1f3e973e8e513ee48197b78b171117aa65437
Last-Railgun <3138304458@qq.com>
2021-09-08 22:46:54 +0800

    显示地图,更改初始位置为当前定位

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -14,6 +14,21 @@ export default {
     map.centerAndZoom(new BMap.Point(116.713637,40.111977), 20);  // 初始化地图,设置中心点坐标和地图级别
     map.setCurrentCity("北京");          // 设置地图显示的城市 此项是必须设置的
     map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+
+    var geolocation = new BMap.Geolocation();
+    // 开启SDK辅助定位
+    geolocation.enableSDKLocation();
+    geolocation.getCurrentPosition(function(r){
+      if(this.getStatus() == BMAP_STATUS_SUCCESS){
+        var mk = new BMap.Marker(r.point);
+        map.addOverlay(mk);
+        map.panTo(r.point);
+        alert('您的位置：' + r.point.lng + ',' + r.point.lat);
+      }
+      else {
+        alert('failed' + this.getStatus());
+      }
+    });
   }
 }
 </script>
